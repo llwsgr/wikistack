924237f99e31910a1b9eb3c6d24fbd34bf5ed002
Linwei Liu <llwsgr@hotmail.com>
2020-03-12 15:25:33 -0400

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-// const express = require('express');
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// const app = express();
-// app.use(morgan('dev'));
-// app.use(express.static(__dirname + '/public'));
-// app.use(bodyParser.urlencoded({extended: false}))
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const layout = require('./views/layout.js')
+const { db } = require('./models');
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
-// app.get('/',(req,res)=>{
-//     res.send(console.log('hello world!!'))
-// });
+const PORT = 3000;
+const syncDb= async()=>{
+  await db.sync({force: true})
+  // await db.Page.sync()
+  // await db.User.sync()
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  })
+}
+syncDb();
 
-// const PORT = 1337;
-// app.listen(PORT, () => {
-//   console.log(`App listening in port ${PORT}`);
-// });
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get('/',(req,res)=>{
+    res.send(layout('!!!hello world!!!!'));
+});
+
+
